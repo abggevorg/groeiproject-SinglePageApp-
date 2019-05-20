5ff6dbee4048ec8f1b9286fc26dc2c79c8d5497f
Ownerszz <tneownerszz@gmail.com>
2019-05-20 17:52:01 +0200

    Created REST functionality

--- src/js/restclient.js
+++ src/js/restclient.js
@@ -1,1 +1,33 @@
-export function makeREStcall(callback, error_callback, url, ...options) {}
+export async function makeREStcall( url, options) {
+    try {
+        let response = await fetch(url, options);
+        if (!response.ok) {
+            throw Error("fetch failed");
+        }
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+export function RESToptionsBuilder(method,instance){
+    let options;
+    if (method.toUpperCase() == "POST"){
+        options =
+            {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(person)
+            };
+    }else{
+        options = 
+        {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    }
+    return options;
+}
