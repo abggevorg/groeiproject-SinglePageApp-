a358849d89051c9f6fb73217d3507add80f7e7c0
Ownerszz <tneownerszz@gmail.com>
2019-05-20 17:54:11 +0200

    Update restclient.js

--- src/js/restclient.js
+++ src/js/restclient.js
@@ -1,33 +1,31 @@
-export async function makeREStcall( url, options) {
-    try {
-        let response = await fetch(url, options);
-        if (!response.ok) {
-            throw Error("fetch failed");
-        }
-        return response;
-    } catch (error) {
-        console.log(error);
+export async function makeREStcall(url, options) {
+  try {
+    let response = await fetch(url, options);
+    if (!response.ok) {
+      throw Error("fetch failed");
     }
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
-export function RESToptionsBuilder(method,instance){
-    let options;
-    if (method.toUpperCase() == "POST"){
-        options =
-            {
-                method: method,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(person)
-            };
-    }else{
-        options = 
-        {
-            method: method,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-    }
-    return options;
+export function RESToptionsBuilder(method, instance) {
+  let options;
+  if (method.toUpperCase() == "POST") {
+    options = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(instance)
+    };
+  } else {
+    options = {
+      method: method,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
+  return options;
 }
