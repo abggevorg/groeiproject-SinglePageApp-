5370b109057cb03c3de8948ebb742fe2a71dd259
abggevorg <36225684+abggevorg@users.noreply.github.com>
2019-05-21 17:53:59 +0200

    secton fixed

--- src/js/page.js
+++ src/js/page.js
@@ -1,3 +1,13 @@
 export function setPageContent(selectedNavItem) {
   console.log(selectedNavItem.innerText.toLowerCase());
+
+  for (let section of document.getElementsByTagName("section")) {
+    console.log(section.id.toLowerCase());
+    if (section.id.toLowerCase() === selectedNavItem.innerText.toLowerCase()) {
+      section.style.display = "block";
+      console.log("gelukt");
+    } else {
+      section.style.display = "none";
+    }
+  }
 }
