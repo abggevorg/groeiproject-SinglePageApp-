c1950619715448da134c3e4510e7ffc561f63b3d
Ownerszz <tneownerszz@gmail.com>
2019-05-21 16:17:16 +0200

    fixed home selected onLoad

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ var fatching = func.fetchFirstTodo();
 fatching.then(data => console.log(data));
 
 */
+function onLoadFunctions() {
+  let home = document.getElementById("#start");
+  home.className = "nav-link active";
+}
+window.onload = onLoadFunctions;
+
 import "./css/style.css";
 import "bootstrap/dist/css/bootstrap.css";
 import * as REST from "./js/restclient";
