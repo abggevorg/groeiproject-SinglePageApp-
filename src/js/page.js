863e404de8babdab067ebb2464fd8cc43d806909
abggevorg <36225684+abggevorg@users.noreply.github.com>
2019-05-22 14:53:06 +0200

    Update page.js

--- src/js/page.js
+++ src/js/page.js
@@ -108,6 +108,15 @@ function setZoekContent() {
   let html = `
   <div class="container justify-content">
   <h1>Zoeken</h1>
+
+  
+<div class="input-group mb-3">
+<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+<div class="input-group-append">
+  <button class="btn btn-outline-secondary" type="button">Zoeken</button>
+</div>
+</div>
+
   <table class="table">
   <thead>
     <tr>
