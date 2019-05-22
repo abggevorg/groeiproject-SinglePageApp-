c933918573871cad852955c314bf06c029c8c996
abggevorg <36225684+abggevorg@users.noreply.github.com>
2019-05-22 14:57:36 +0200

    Update page.js

--- src/js/page.js
+++ src/js/page.js
@@ -111,7 +111,7 @@ function setZoekContent() {
 
   
 <div class="input-group mb-3">
-<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+<input type="text" class="form-control" placeholder="Zoeken..." aria-label="Zoeken..." aria-describedby="basic-addon2">
 <div class="input-group-append">
   <button class="btn btn-outline-secondary" type="button">Zoeken</button>
 </div>
