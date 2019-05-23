ede8d1c5a4bdf57fe95ed084016a936ee26ce430
Ownerszz <tneownerszz@gmail.com>
2019-05-24 00:27:01 +0200

    Fixed footer

--- src/app.js
+++ src/app.js
@@ -2,14 +2,7 @@ import "./css/style.css";
 import "bootstrap/dist/css/bootstrap.css";
 import * as navManipulation from "./js/layout/navbar";
 
-/*import "./css/style.css";
 
-import * as func from "./js/restclient";
-
-var fatching = func.fetchFirstTodo();
-fatching.then(data => console.log(data));
-
-*/
 function onLoadFunctions() {
   navManipulation.addEventsToNavbar();
 }
